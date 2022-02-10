51fea8e03f5babb1201abef46b7ba99d1dee536a
sahil <sahilkholia9@gmail.com>
2022-02-10 15:18:44 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { Question } from './question';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'internationalize';
+  public questions:any;
+  constructor(public translate: TranslateService, private http: HttpClient ){
+    translate.addLangs(['en', 'hn', 'sp']);
+    translate.setDefaultLang('en');
+    this.questions=[];
+    this.currQuestions=[];
+  }
+
+
+
+  private url:string="http://localhost:8080/survey";
+  public currQuestions:string[];
+  getQuestions(): Observable<Question[]>{
+    return this.http.get<Question[]>(this.url);
+  }
+
+  ngOnInit(){
+    this.getQuestions().subscribe((data:any) => {
+      this.questions=data;
+      
+    });
+    
+  }
+
+  changeLang(){
+
+  }
+  
+
 }
---
+++ src/app/question.ts
@@ -0,0 +1,6 @@
+export interface Question{
+    id: number,
+    english: string,
+    hindi: string,
+    spanish: string
+}
